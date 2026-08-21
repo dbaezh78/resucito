8c594d99ba588c17693f849554861c81f1729352
dbaezh78 <dbaezh78@gmail.com>
2026-08-21 08:55:05 -0400

    update

--- copy_assets.cjs
+++ copy_assets.cjs
@@ -42,7 +42,7 @@ folders.forEach(folder => {
 });
 
 // Copy individual files
-const files = ['manifest.json', 'sw.js'];
+const files = ['manifest.json', 'sw.js', '.nojekyll'];
 files.forEach(file => {
     const src = path.resolve(__dirname, file);
     const dest = path.resolve(distPath, file);
